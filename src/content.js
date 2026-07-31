a83113af8faf625117ee4bd1527e49b2197a5830
melnyk01 <223485602+melnyk01@users.noreply.github.com>
2026-07-31 21:24:41 +0200

    Improve restaurant lage layout and styling

--- src/content.js
+++ src/content.js
@@ -3,85 +3,102 @@ export function displayPageContent() {
     const content = document.querySelector('#content');
 
     // Header
-    const header = document.createElement('h1');
-    const headerText = document.createElement('h2');
-    header.textContent = 'Welcome to the Vaddy\'s';
-    headerText.textContent = 'Fresh ingredients. Homemade recipes.'
+    const header = document.createElement('header');
+    const h1 = document.createElement('h1');
+    const h1Text = document.createElement('h2');
+    h1.textContent = 'Welcome to the Vaddy\'s';
+    h1Text.textContent = 'Fresh ingredients. Homemade recipes.'
+
+    header.appendChild(h1);
+    header.appendChild(h1Text);
 
     // Today's special
     const todaysSpecial = document.createElement('div');
+    todaysSpecial.className = 'special';
     const h2 = document.createElement('h2');
-    const mealImage = document.createElement('img');
-    const mealDescription = document.createElement('div');
+    const specialImage = document.createElement('img');
+    const specialDescription = document.createElement('div');
     const mealPrice = document.createElement('div');
     h2.textContent = 'Today\'s special';
-    mealImage.src = frenchToast;
-    mealDescription.textContent = 'Golden brioche served with fresh fruits, maple syrup and powdered sugar.';
+    specialImage.src = frenchToast;
+    specialDescription.textContent = 'Golden brioche served with fresh fruits, maple syrup and powdered sugar.';
     mealPrice.textContent = '$9.90'
     todaysSpecial.appendChild(h2);
-    todaysSpecial.appendChild(mealImage);
-    todaysSpecial.appendChild(mealDescription);
+    todaysSpecial.appendChild(specialImage);
+    todaysSpecial.appendChild(specialDescription);
     todaysSpecial.appendChild(mealPrice);
 
     content.appendChild(header);
     content.appendChild(todaysSpecial);
 
+    //
+    function createMeal(meal) {
+        const card = document.createElement('div');
+        card.className = 'card';
+        const mealName = document.createElement('div');
+        const mealDescription = document.createElement('div');
+        const mealPrice = document.createElement('div');
+        const mealCategory = document.createElement('div');
+        mealName.textContent = meal.name;
+        mealDescription.textContent = meal.description;
+        mealPrice.textContent = meal.price;
+        mealCategory.textContent = meal.category;
+
+        card.appendChild(mealName);
+        card.appendChild(mealDescription);
+        card.appendChild(mealPrice);
+
+        menu.appendChild(card);
+    }
+
     // Menu
     const menu = document.createElement('div');
-    let menus = [
-        [
-            {
-                name: 'Guláš',
-                price: '$9.90',
-                category: 'Popular'
-            },
-            {
-                name: 'Svickova',
-                price: '$9.90',
-                category: 'Popular'
-            },
-            {
-                name: 'Pancakes',
-                price: '$9.90',
-                category: 'Popular'
-            },
-        ],
-        [
-            {
-                name: 'Borshch',
-                price: '$9.90',
-                category: "Chef's Picks",
-            },
-            {
-                name: 'Smazak',
-                price: '$9.90',
-                category: "Chef's Picks",
-            },
-            {
-                name: 'Pasta',
-                price: '$9.90',
-                category: "Chef's Picks",
-            }
-        ]
-    ]
-    for (let category of menus) {
-        const meals = document.createElement('ul');
-        for (const meal of category) {
-            const card = document.createElement('div');
-            const mealCategory = document.createElement('div');
-            const mealName = document.createElement('div');
-            const mealPrice = document.createElement('div');
-            mealCategory.textContent = meal.category;
-            mealName.textContent = meal.name;
-            mealPrice.textContent = meal.price;
-
-            card.appendChild(mealCategory);
-            card.appendChild(mealName);
-            card.appendChild(mealPrice);
-
-            menu.appendChild(card);
+    menu.className = 'menu';
+    let meals = [
+        {
+            name: 'Guláš',
+            description: 'A hearty beef stew slow-cooked with onions, paprika, and spices, typically served with bread dumplings.',
+            price: '$9.90',
+            category: 'Popular'
+        },
+        {
+            name: 'Svíčková',
+            description: 'Tender beef in a creamy root vegetable sauce, traditionally served with bread dumplings, whipped cream, and cranberries.',
+            price: '$9.90',
+            category: 'Popular'
+        },
+        {
+            name: 'Pancakes',
+            description: 'Thin or fluffy cakes made from batter and served with sweet toppings like syrup, fruit, chocolate, or whipped cream.',
+            price: '$9.90',
+            category: 'Popular'
+        },
+        {
+            name: 'Borshch',
+            description: 'A traditional beetroot soup with vegetables, often containing beef and served with sour cream.',
+            price: '$9.90',
+            category: "Chef's Picks",
+        },
+        {
+            name: 'Smažák',
+            description: 'A breaded and deep-fried cheese, usually served with fries, tartar sauce, and a side salad.',
+            price: '$9.90',
+            category: "Chef's Picks",
+        },
+        {
+            name: 'Pasta',
+            description: 'Cooked noodles served with a variety of sauces, such as tomato, cream, pesto, or meat-based sauces.',
+            price: '$9.90',
+            category: "Chef's Picks",
         }
+    ]
+    for (const meal of meals) {
+        createMeal(meal);
     }
 
     content.appendChild(menu);
 }
+
+const body = document.querySelector('body');
+const footer = document.createElement('footer');
+body.appendChild(footer);
