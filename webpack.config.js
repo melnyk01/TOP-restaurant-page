4ad6523d9a3bb91370c5cffee4a16e3ef7f48680
melnyk01 <223485602+melnyk01@users.noreply.github.com>
2026-08-03 06:32:24 +0200

    Add webpack scripts

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 
 
 export default {
-    mode: "development",
+    mode: "production",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
